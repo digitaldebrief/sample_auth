0fd5d66f4cbd4aa89fcdf6aa3ec8f29283e0ed60
Aviendha Stark Lannister <aviendhasl@digitaldebrief.com>
2023-05-31 05:09:29 +0000

    Add config values to Dynaconf call.

--- sample_auth/base.py
+++ sample_auth/base.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 def create_app(**config):
     app = Flask(__name__)
-    FlaskDynaconf(app)  # config managed by Dynaconf
+    FlaskDynaconf(app, **config)  # config managed by Dynaconf
     app.config.load_extensions(
         "EXTENSIONS"
     )  # Load extensions from settings.toml
