118552738e64bb6ae4c349e5b03b24fb9822f52f
Aviendha Stark Lannister <aviendhasl@digitaldebrief.com>
2023-06-01 05:15:13 +0000

    Fix some linting errors

--- sample_auth/ext/restapi/resources.py
+++ sample_auth/ext/restapi/resources.py
@@ -1,13 +1,11 @@
 from flask import abort, jsonify
 from flask_restful import Resource, reqparse
-from flask_simplelogin import login_required
 
-from sample_auth.models import Product
 from sample_auth.ext.database import db
+from sample_auth.models import Product
 
 
 class ProductResource(Resource):
-
     def get(self):
         products = Product.query.all() or abort(204)
         return jsonify(
@@ -24,17 +22,23 @@ class ProductResource(Resource):
         curl -XPOST localhost:5000/api/v1/product/ \
          -H "Authorization: Basic YWRtaW46MTIzNAo" \
          -H "Content-Type: application/json" \
-         -d '{"name":"Rye", "price":5, "description":"This bread has a grain with strong flavor"}'
+         -d '{"name":"Rye", "price":5, \
+            "description":"This bread has a grain with strong flavor"}'
         """
         parser = reqparse.RequestParser()
-        parser.add_argument('name', type=str)
-        parser.add_argument('price', type=float)
-        parser.add_argument('description', type=str)
+        parser.add_argument("name", type=str)
+        parser.add_argument("price", type=float)
+        parser.add_argument("description", type=str)
         args = parser.parse_args()
-        new_product = Product(name=args['name'], price=args['price'], description=args['description'])
+        new_product = Product(
+            name=args["name"],
+            price=args["price"],
+            description=args["description"],
+        )
         db.session.add(new_product)
         db.session.commit()
         return jsonify(new_product.to_dict()) if new_product else abort(500)
+
 
 class ProductItemResource(Resource):
     def get(self, product_id):
